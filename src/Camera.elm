5902ad6cdbf1321505c532a01c15e286118afb4a
schroffl <hannes.holzwarth@me.com>
2017-10-28 17:19:39 +0200

    Add function to update the perspective matrix based on the window size

--- src/Camera.elm
+++ src/Camera.elm
@@ -2,6 +2,7 @@ module Camera exposing (..)
 
 import Math.Vector3 as Vector3 exposing (Vec3, vec3)
 import Math.Matrix4 as Matrix4 exposing (Mat4)
+import Window
 
 
 type alias Orientation =
@@ -43,6 +44,24 @@ makeViewMatrix cam =
             |> Matrix4.rotate rotation (vec3 0 1 0)
             |> Matrix4.rotate tilt (vec3 xAxis 0 zAxis)
             |> Matrix4.translate (Vector3.scale 2 << Vector3.negate <| cam.position)
+
+
+updatePerspective : Window.Size -> Camera -> Camera
+updatePerspective size cam =
+    let
+        width =
+            toFloat size.width
+
+        height =
+            toFloat size.height
+
+        ratio =
+            Debug.log "Ratio" (width / height)
+    in
+        { cam
+            | perspectiveMatrix =
+                Matrix4.makePerspective 45 (ratio) 0.01 100
+        }
 
 
 applyMouseMovement : MouseMovement -> Camera -> Camera
